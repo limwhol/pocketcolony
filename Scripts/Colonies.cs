f3acbc8eca9bd2a9c2d1c9e043388ac04dcb7225
limwhol <57886952+limwhol@users.noreply.github.com>
2023-02-23 21:58:22 +0800

    Add files via upload

---
+++ Scripts/Colonies.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Colony Inventory", menuName = "ANT ASSETS/Colony Inventory")]
+
+public class Colonies:ScriptableObject
+{
+    public List<Colony> antColonies;
+}
